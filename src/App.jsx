4ce183e1f88eb9c7acf4fbd968dab2e6e11fa411
Rendrez <rendrez0027@gmail.com>
2022-10-10 15:18:05 +0700

    memainkan componen dan ikon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { IconBrandFacebook, IconBrandTwitter } from "@tabler/icons"
+import clsx from "clsx";
+
 export default function App() {
-    return <div className="bg-blue-600 grid place-content-center min-h-screen">
-        <h1 className="text-xl">
-            Learn react
-        </h1>
+    const type = 'submit';
+    const onClick = () => console.log('login');
+    return <div className="bg-slate-900 grid place-content-center min-h-screen">
+        <div className={"flex gap-x-2"}>
+            <Button {...{type, onClick }}>
+                <IconBrandFacebook/>
+                Register
+            </Button>
+            <Button className='bg-red-400'>
+                <IconBrandTwitter/>
+                Login
+            </Button>
+        </div>
     </div>
+}
+
+function Button(props) {
+    const {className = 'bg-blue-600', text, children} = props;
+    return (        
+        <button {...props} className={clsx(
+            className,
+            '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 text-white px-4 py-2 rounded'
+        )}>
+            {text || children}
+        </button>
+    )
 }
